edff64d38d321b8c982bd1ae8ed7020e326c0192
Jaeyoung <jyko1101@gmail.com>
2024-11-25 21:04:04 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import usersRouter from './routes/users.router.js';
+import ErrorHandlingMiddleware from './middlewares/errorHandling.middleware.js';
 
 const app = express();
 const PORT = 3019;
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use('/api', usersRouter);
+app.use(ErrorHandlingMiddleware);
+
 
 app.listen(PORT, () => {
   console.log(PORT, 'port opened == item-simulator Server running!');

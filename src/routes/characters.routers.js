c60e9100ac1d126c338bd97531b0eeabada78c4e
Jaeyoung <jyko1101@gmail.com>
2024-11-26 17:23:29 +0900

    feat: 상세 조회

--- src/routes/characters.routers.js
+++ src/routes/characters.routers.js
@@ -21,7 +21,7 @@ const router = express.Router();
 // 캐릭터 생성 성공시, 캐릭터 ID를 response로 돌려받기
 //====================================================================================================================
 //====================================================================================================================
-router.post('/character/create', authMiddleware, async (req, res, next) => {
+router.post('/characters/create', authMiddleware, async (req, res, next) => {
     try {
         const { user_id } = req.user;
         const { character_name, character_type } = req.body;
@@ -52,7 +52,6 @@ router.post('/character/create', authMiddleware, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
 });
 
 
@@ -65,13 +64,13 @@ router.post('/character/create', authMiddleware, async (req, res, next) => {
 // 삭제 성공시, 메시지
 //====================================================================================================================
 //====================================================================================================================
-router.post('/character/delete', authMiddleware, async (req, res, next) => {
+router.delete('/characters/:character_id', authMiddleware, async (req, res, next) => {
     try {
-        const { character_id } = req.body;
+        const { character_id } = req.params;
         const { user_id } = req.user;
         const character = await prisma.characters.findFirst({
             where: {
-                character_id,
+                character_id: +character_id,
             },
         });
         if (!character)
@@ -82,47 +81,60 @@ router.post('/character/delete', authMiddleware, async (req, res, next) => {
 
         // 캐릭터 삭제
         await prisma.characters.delete({
-            where: { character_id },
+            where: { character_id: +character_id },
         });
 
+        return res.status(200).json({ message: '[Success] deletion completed' });
     } catch (error) {
         next(error);
     }
+});
 
+
+
+//====================================================================================================================
+//====================================================================================================================
+// get character API : 캐릭터 상세 조회
+// character_id를 param으로 해당 캐릭터 상세 정보 조회
+// 자신의 캐릭터를 조회하는 경우, 모든 세부 정보 보기
+// 로그인하지 않았거나 다른 유저가 내 캐릭터 조회 시, character_name과 character_type 만 조회
+//====================================================================================================================
+//====================================================================================================================
+router.get('/characters/:character_id', authMiddleware, async (req, res, next) => {
+    try {
+        const { character_id } = req.params;
+        const character = await prisma.characters.findFirst({
+            where: {
+                character_id: +character_id,
+            },
+            select: {
+                character_name: true,
+                character_type: true,
+                characterStats: {
+                    select: {
+                        hp: true,
+                        mp: true,
+                        level: true,
+                        attack: true,
+                        defense: true,
+                    }
+                },
+                // 자신의 캐릭터일 경우만 재화 까지 표시
+                // ...user_id === character.user_id && {
+
+                // },
+            },
+        });
+        if (!character)
+            return res.status(404).json({ message: '[Not Found] character not found' });
+
+        return res.status(200).json({ data: character });
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default router;
 
 
 
-
-
-
-
-router.post('/character/delete', authMiddleware, async (req, res, next) => {
-    const { character_id } = req.body;
-    const { user_id } = req.user; // authMiddleware를 통해 설정된 사용자 정보
-
-    try {
-        // 캐릭터가 해당 사용자 계정에 속하는지 확인
-        const character = await prisma.characters.findFirst({
-            where: {
-                character_id,
-                user_id,
-            },
-        });
-
-        if (!character) {
-            return res.status(404).json({ message: '[Not Found] character not found or unauthorized' });
-        }
-
-        // 캐릭터 삭제
-        await prisma.characters.delete({
-            where: { character_id },
-        });
-
-        return res.status(200).json({ message: '[Success] character deleted.' });
-    } catch (error) {
-        next(error);
-    }
-});
---
+++ src/routes/items.routers.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+
+const router = express.Router();
+
+
+
+export default router;

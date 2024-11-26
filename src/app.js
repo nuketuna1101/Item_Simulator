56c41739320ac7414117377b4a962efb93392ee2
Jaeyoung <jyko1101@gmail.com>
2024-11-26 13:06:19 +0900

    feat: logging middleware

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import usersRouter from './routes/users.router.js';
+// middlewares
+import LoggingMiddleware from './middlewares/logging.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/errorHandling.middleware.js';
+import usersRouter from './routes/users.routers.js';
 
 const app = express();
 const PORT = 3019;
 
 // Middlewares
+app.use(LoggingMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', usersRouter);
